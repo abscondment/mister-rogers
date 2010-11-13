77b7fd55078bf2eb43925e859203236acc241571
Brendan Ribera <brendan.ribera@gmail.com>
2010-11-13 15:16:15 -0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject fred-rogers "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [aleph "0.1.3-SNAPSHOT"]
+                 [clj-kdtree "0.1.1"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
