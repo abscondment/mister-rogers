d1a476e29c818f936f46b437b1d8283d7dbc25dd
Brendan Ribera <brendan.ribera@gmail.com>
2010-11-13 15:41:10 -0800

    Correct namespacing; cast read strings into numbers.

--- src/fred/rogers/arborist.clj
+++ src/fred/rogers/arborist.clj
@@ -1,15 +1,17 @@
-(ns fred-rogers.arborist
+(ns fred.rogers.arborist
   (:require [kdtree :as kdtree]
              [clojure.contrib.string :as str])
   (:use [clojure.java.io :only [reader]]))
 
 (defn- reload-data [file]
   (let [lines (-> file reader line-seq)]
-    (map #(with-meta (vec (next %)) {:id (first %)})
+    (map #(with-meta
+            (vec (map (fn [v] (Double/parseDouble v)) (next %)))
+            {:id (Integer/parseInt (first %))})
          (map #(str/split #"[\s]+" %) lines))))
 
 (defn update-tree [file]
   (let [new (kdtree/build-tree
              (reload-data file))]
-    (dosync (ref-set fred-rogers.core/*tree* new))))
+    (dosync (ref-set fred.rogers/*tree* new))))
 
