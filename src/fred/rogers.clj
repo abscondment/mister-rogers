62a433a2e1fd48f66193621f032d7bbd6e03da9c
Brendan Ribera <brendan.ribera@gmail.com>
2010-11-13 15:40:29 -0800

    Fix two tree issues: First, define the tree ref in the fred.rogers namespace before attepmting to require libraries that use it. Second, actually pass in the sample data file.

--- src/fred/rogers.clj
+++ src/fred/rogers.clj
@@ -1,11 +1,12 @@
-(ns fred-rogers.core
-  (:require
-   [fred-rogers.arborist :as arborist]
-   [fred-rogers.server :as server]))
-
+(ns fred.rogers)
 (def *tree* (ref '()))
+
+(ns fred.rogers
+  (:require
+   [fred.rogers.arborist :as arborist]
+   [fred.rogers.server :as server]))
 
 (defn -main [& args]
   (do
-    (arborist/update-tree)
+    (arborist/update-tree "seattle-sample.dat")
     (server/run)))
