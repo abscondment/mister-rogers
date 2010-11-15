524eafd13deebc9f0786486d5bef6f3e5178c4b0
Brendan Ribera <brendan.ribera@gmail.com>
2010-11-15 09:35:35 -0800

    Speling :(

--- config.clj
+++ config.clj
@@ -1,7 +1,7 @@
 {
  ;; File is rows of space-delimited id, lat, and lon.
  :data-file "seattle-sample.dat"
- ;; Attepmt to re-read the data file every 5 minutes
+ ;; Attempt to re-read the data file every 5 minutes
  :data-update-interval 300
  ;; Return only the nearest neighbor if size is unspecified.
  :default-result-size 1
