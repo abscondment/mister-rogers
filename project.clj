4783a4b5507eb16751c107b8a333745890fa0613
abscondment <brendan.ribera@gmail.com>
2010-11-28 15:29:35 -0800

    Rudimentary routing and error handling. Outputting json content.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [aleph "0.1.3-SNAPSHOT"]
                  [ring/ring-core "0.3.3"]
-                 [clj-kdtree "0.1.1"]]
+                 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
+                 [clj-kdtree "0.1.1"]
+                 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :main mister.rogers)
--- src/mister/rogers/server.clj
+++ src/mister/rogers/server.clj
@@ -1,29 +1,49 @@
 (ns mister.rogers.server
   (:require [clojure.contrib.string :as str]
-            [kdtree :as kdtree])
+            [ring.middleware.params :as params]
+            [kdtree :as kdtree]
+            [org.danlarkin.json :as json])
   (:use [lamina core]
-        [aleph http]))
+        [aleph http]
+        [net.cgrand moustache]))
 
-(defn- query-map [request]
-  (reduce
-   (fn [result [key val]]
-     (assoc result key val))
-   {}
-   (map #(str/split #"=" %)
-        (str/split #"&" (or (request :query-string) "")))))
+(defn- to-double [#^String string]
+  (try
+    (Double/parseDouble string)
+    (catch Exception _)))
+
+(defn- to-int [#^String string]
+  (try
+    (Integer/parseInt string)
+    (catch Exception _)))
 
 (defn- find-neighbors [channel request]
-  (let [params (query-map request)
-        point  [(Double/parseDouble (get params "lat"))
-                (Double/parseDouble (get params "lon"))]
-        n (Integer/parseInt (or (get params "n") "5"))]
-   (enqueue channel
-            {:status 200
-             :headers {"content-type" "text/html"}
-             :body (str
-                    (seq (kdtree/nearest-neighbor
-                          @mister.rogers/neighborhood
-                          point
-                          n)))})))
+  (let [params (:params request)
+        location  [(to-double (get params "lat"))
+                   (to-double (get params "lon"))]
+        n (or (to-int (get params "n")) 5)]
+    (enqueue channel
+             (if (not-any? nil? location)
+               ;; Fetch response
+               {:status 200
+                :headers {"content-type" "text/json"}
+                :body (json/encode
+                       (map
+                        #(merge % (meta (:point %)))
+                        (kdtree/nearest-neighbor @mister.rogers/neighborhood
+                                                 location
+                                                 n)))}
+               ;; Bad location
+               {:status 400
+                :headers {"content-type" "text/plain"}
+                :body (str {:error "Bad location"})}
+               ))))
 
-(defn run [] (start-http-server find-neighbors {:port 8080}))
+(def handler
+     (app ["stats"]
+          {:get "STATS stub"}
+          
+          ["neighbors" "nearest"] {:get (params/wrap-params
+                                         (wrap-aleph-handler find-neighbors))}))
+
+(defn run [] (start-http-server (wrap-ring-handler handler) {:port 8080}))
